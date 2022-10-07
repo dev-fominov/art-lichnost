5b941c119c18d3f659dc7293d830cf11366177cf
dev-fominov <dev-fominov@yandex.by>
2022-10-07 12:17:33 +0300

    add page merch

--- app/content/themes/gulp-theme/api/art-api.php
+++ app/content/themes/gulp-theme/api/art-api.php
@@ -1,78 +1,133 @@
 <?php
 
 add_action('rest_api_init', function () {
-	// register_rest_route('art/v1', 'camp/page', [
-	// 	'methods' => 'GET',
-	// 	'callback' => 'art_camp_page',
-	// ]);
-	// register_rest_route('art/v1', 'camp/card', [
-	// 	'methods' => 'GET',
-	// 	'callback' => 'art_camp_card',
-	// ]);
-	// register_rest_route('art/v1', 'menu', [
-	// 	'methods' => 'GET',
-	// 	'callback' => 'art_menu',
-	// ]);
 
+	$prefix = 'art/v1';
 
-	register_rest_route('art/v1', 'page/contacts', [
+	register_rest_route($prefix, 'page/contacts', [
 		'methods' => 'GET',
 		'callback' => 'art_page_contacts',
 		'permission_callback' => '__return_true'
 	]);
-	register_rest_route('art/v1', 'page/home', [
+	register_rest_route($prefix, 'page/home', [
 		'methods' => 'GET',
 		'callback' => 'art_page_home',
 		'permission_callback' => '__return_true'
 	]);
-	register_rest_route('art/v1', 'page/blogs', [
+	register_rest_route($prefix, 'page/blogs', [
 		'methods' => 'GET',
 		'callback' => 'art_page_blogs',
 		'permission_callback' => '__return_true'
 	]);
-	register_rest_route('art/v1', 'page/blogs/(?P<slug>[a-zA-Z0-9-]+)', [
+	register_rest_route($prefix, 'page/blogs/(?P<slug>[a-zA-Z0-9-]+)', [
 		'methods' => 'GET',
 		'callback' => 'art_page_blogs_post',
 		'permission_callback' => '__return_true'
 	]);
-	register_rest_route('art/v1', 'page/psychologist', [
+	register_rest_route($prefix, 'page/psychologist', [
 		'methods' => 'GET',
 		'callback' => 'art_page_psychologist',
 		'permission_callback' => '__return_true'
 	]);
 
 	// Лагеря
-	register_rest_route('art/v1', 'page/camp', [
+	register_rest_route($prefix, 'page/camp', [
 		'methods' => 'GET',
 		'callback' => 'art_page_camp',
 		'permission_callback' => '__return_true'
 	]);
 
-	register_rest_route('art/v1', 'camp/filter', [
+	register_rest_route($prefix, 'camp/filter', [
 		'methods' => 'GET',
 		'callback' => 'art_camp_filter',
 		'permission_callback' => '__return_true'
 	]);
 
-	register_rest_route('art/v1', 'page/skills', [
+	register_rest_route($prefix, 'page/skills', [
 		'methods' => 'GET',
 		'callback' => 'art_page_skills',
 		'permission_callback' => '__return_true'
 	]);
 
-	register_rest_route('art/v1', 'page/professions', [
+	register_rest_route($prefix, 'page/professions', [
 		'methods' => 'GET',
 		'callback' => 'art_page_professions',
 		'permission_callback' => '__return_true'
 	]);
 
-	register_rest_route('art/v1', 'page/tourist-holidays', [
+	register_rest_route($prefix, 'page/tourist-holidays', [
 		'methods' => 'GET',
 		'callback' => 'art_page_tourist_holidays',
 		'permission_callback' => '__return_true'
 	]);
+
+	register_rest_route($prefix, 'page/merch', [
+		'methods' => 'GET',
+		'callback' => 'art_page_merch',
+		'permission_callback' => '__return_true'
+	]);
 });
 
+
+function art_page_merch()
+{
+	$data = [];
+
+	$my_page = get_page_by_path('merch', OBJECT, 'page');
+	$id_page = $my_page->ID;
+
+	$post = get_post($id_page);
+	$title = apply_filters('the_content', $post->post_content);
+
+	$array_description_merch = get_field('kak_kupit', $id_page);
+	foreach ($array_description_merch as $item) {
+		$description_merch[] = $item['punkty'];
+	}
+
+	$description = [
+		'title_merch' => get_field('title_merch', $id_page),
+		'description_merch' => $description_merch,
+	];
+
+	$array_merch = get_field('aleksandr-merch', $id_page) ? get_field('aleksandr-merch', $id_page) : null;
+	if ($array_merch) {
+		foreach ($array_merch as $item) {
+			$gallery_images = $item['gallery_images'];
+			$images = null;
+			foreach ($gallery_images as $item_image) {
+				$images[] = [
+					'url' => $item_image['images']['url'],
+					'alt' => $item_image['images']['title']
+				];
+			}
+
+			$array_size = $item['razmery'];
+			$size = null;
+			foreach ($array_size as $item_size) {
+				$stock = $item_size['nalichie'][0] ? $item_size['nalichie'][0] : 0;
+				$size[] = [
+					'title' => $item_size['nazvanie'],
+					'stock' => (int)$stock
+				];
+			}
+
+			$merch[] = [
+				'title' => $item['nazvanie'],
+				'description' => $item['opisanie'],
+				'price' => $item['price-merch'],
+				'gallery' => $images,
+				'size' => $size,
+			];
+		}
+	}
+
+	$data['id_page'] = $id_page;
+	$data['title'] = $title;
+	$data['description'] = $description;
+	$data['merch'] = $merch;
+
+	return $data;
+}
 
 function art_page_skills()
 {
@@ -880,225 +935,3 @@ function art_page_psychologist()
 
 	return $data;
 }
-
-
-
-
-
-
-
-// function art_menu()
-// {
-// 	$data = [];
-
-// 	$locations = get_nav_menu_locations()['header_menu'];
-// 	$items = wp_get_nav_menu_items( $locations );
-	
-// 	$menu = [];
-
-// 	foreach($items as $item) {
-// 		$menu[] = [
-// 			'ID' => $item->ID,
-// 			'parent_id' => intval($item->menu_item_parent),
-// 			'title' => $item->title,
-// 			'url' => $item->url
-// 		];
-// 	}
-
-
-// 	$data['menu'] = $menu;
-
-// 	return $data;
-// }
-
-
-// function art_camp_page()
-// {
-// 	$args = [
-// 		'post_type' => 'camp'
-// 	];
-
-// 	$camps = get_posts($args);
-// 	$data = [];
-// 	$i = 0;
-
-// 	foreach ($camps as $camp) {
-// 		$id = $camp->ID;
-
-// 		$ages = [];
-// 		$sections = [];
-// 		$cardProfi = [];
-// 		$cardProfstart = [];
-
-// 		$campAges = get_the_terms($id, 'camp-ages');
-// 		$campSection = get_the_terms($id, 'camp-section');
-
-// 		foreach ($campAges as $age) {
-// 			$ages[] = $age->name;
-// 		}
-// 		foreach ($campSection as $section) {
-// 			$sections[] = [
-// 				'name' => $section->name,
-// 				'slug' => $section->slug,
-// 			];
-// 		}
-
-// 		// Added acf to api
-// 		$imageMiniatyura = get_field('image_miniatyura', $id);
-// 		$imageInfo = [
-// 			'url' => $imageMiniatyura['url'],
-// 			'title' => $imageMiniatyura['title']
-// 		];
-// 		$location = get_field('location', $id);
-// 		$nalichie_sertifikata = get_field('nalichie_sertifikata', $id);
-// 		$change_price = get_field('change_price', $id);
-// 		$price_per_certificate = get_field('price_per_certificate', $id);
-// 		$text_without_action = get_field('text_without_action', $id);
-// 		$description_box_profi = get_field('description_box_profi', $id);
-// 		$description_box_profstart = get_field('description_box_profstart', $id);
-
-// 		if (have_rows('profi_cards', $id)) {
-// 			while (have_rows('profi_cards', $id)) {
-// 				the_row();
-// 				$image_card = get_sub_field('image_card');
-// 				$cardProfi[] = [
-// 					'title_card' => get_sub_field('title_card'),
-// 					'description_card' => get_sub_field('description_card'),
-// 					'nalichie_mest_card' => get_sub_field('nalichie_mest_card'),
-// 					'image_card' => ['url' => $image_card['url'], 'title' => $image_card['title']],
-// 				];
-// 			}
-// 		}
-
-// 		if (have_rows('profstart_cards', $id)) {
-// 			while (have_rows('profstart_cards', $id)) {
-// 				the_row();
-// 				$image_card = get_sub_field('image_card');
-
-// 				$cardProfstart[] = [
-// 					'title_card' => get_sub_field('title_card'),
-// 					'description_card' => get_sub_field('description_card'),
-// 					'nalichie_mest_card' => get_sub_field('nalichie_mest_card'),
-// 					'image_card' => ['url' => $image_card['url'], 'title' => $image_card['title']]
-// 				];
-// 			}
-// 		}
-
-// 		$card_profi_box = ['description_box' => $description_box_profi, 'card' =>  $cardProfi];
-// 		$card_profstart_box = ['description_box' => $description_box_profstart, 'card' =>  $cardProfstart];
-
-// 		// Added to api
-// 		$data[$i]['id'] = $id;
-// 		$data[$i]['title'] = $camp->post_title;
-// 		$data[$i]['camp_ages'] = $ages;
-// 		$data[$i]['camp_section'] = $sections;
-// 		$data[$i]['image_miniatyura'] = $imageInfo;
-// 		$data[$i]['location'] = $location;
-// 		$data[$i]['nalichie_sertifikata'] = $nalichie_sertifikata;
-// 		$data[$i]['change_price'] = $change_price;
-// 		$data[$i]['price_per_certificate'] = $price_per_certificate;
-// 		$data[$i]['text_without_action'] = $text_without_action;
-
-// 		$data[$i]['card_box'] = [$card_profi_box, $card_profstart_box];
-
-// 		$i++;
-// 	}
-
-// 	return $data;
-// }
-
-
-// function art_camp_card()
-// {
-// 	$args = [
-// 		'post_type' => 'camp'
-// 	];
-
-// 	$camps = get_posts($args);
-// 	$data = [];
-// 	$i = 0;
-
-// 	foreach ($camps as $camp) {
-// 		$id = $camp->ID;
-
-// 		$ages = [];
-// 		$periods = [];
-// 		$sections = [];
-// 		$cardProfi = [];
-// 		$cardProfstart = [];
-
-// 		$campAges = get_the_terms($id, 'camp-ages');
-// 		$campPeriod = get_the_terms($id, 'camp-period');
-// 		$campSection = get_the_terms($id, 'camp-section');
-
-// 		foreach ($campAges as $age) {
-// 			$ages[] = $age->name;
-// 		}
-// 		foreach ($campPeriod as $period) {
-// 			$periods[] = $period->name;
-// 		}
-// 		foreach ($campSection as $section) {
-// 			$sections[] = $section->name;
-// 		}
-
-// 		// Added acf to api
-// 		$imageMiniatyura = get_field('image_miniatyura', $id);
-// 		$imageInfo = [
-// 			'url' => $imageMiniatyura['url'],
-// 			'title' => $imageMiniatyura['title']
-// 		];
-// 		$location = get_field('location', $id);
-// 		$nalichie_sertifikata = get_field('nalichie_sertifikata', $id);
-// 		$change_price = get_field('change_price', $id);
-// 		$price_per_certificate = get_field('price_per_certificate', $id);
-// 		$text_without_action = get_field('text_without_action', $id);
-// 		$description_box_profi = get_field('description_box_profi', $id);
-// 		$description_box_profstart = get_field('description_box_profstart', $id);
-
-// 		if (have_rows('profi_cards', $id)) {
-// 			while (have_rows('profi_cards', $id)) {
-// 				the_row();
-// 				$image_card = get_sub_field('image_card');
-// 				$cardProfi[] = [
-// 					'title_card' => get_sub_field('title_card'),
-// 					'description_card' => get_sub_field('description_card'),
-// 					'nalichie_mest_card' => get_sub_field('nalichie_mest_card'),
-// 					'image_card' => ['url' => $image_card['url'], 'title' => $image_card['title']],
-// 				];
-// 			}
-// 		}
-
-// 		if (have_rows('profstart_cards', $id)) {
-// 			while (have_rows('profstart_cards', $id)) {
-// 				the_row();
-// 				$image_card = get_sub_field('image_card');
-
-// 				$cardProfstart[] = [
-// 					'title_card' => get_sub_field('title_card'),
-// 					'description_card' => get_sub_field('description_card'),
-// 					'nalichie_mest_card' => get_sub_field('nalichie_mest_card'),
-// 					'image_card' => ['url' => $image_card['url'], 'title' => $image_card['title']]
-// 				];
-// 			}
-// 		}
-
-// 		// Added to api
-// 		$data[$i]['id'] = $id;
-// 		$data[$i]['title'] = $camp->post_title;
-// 		$data[$i]['camp_ages'] = $ages;
-// 		$data[$i]['camp_period'] = $periods;
-// 		$data[$i]['camp_section'] = $sections;
-// 		$data[$i]['image_miniatyura'] = $imageInfo;
-// 		$data[$i]['location'] = $location;
-// 		$data[$i]['nalichie_sertifikata'] = $nalichie_sertifikata;
-// 		$data[$i]['change_price'] = $change_price;
-// 		$data[$i]['price_per_certificate'] = $price_per_certificate;
-// 		$data[$i]['text_without_action'] = $text_without_action;
-// 		$data[$i]['card_profi_box'] = ['description_box_profi' => $description_box_profi, 'card_profi' =>  $cardProfi];
-// 		$data[$i]['card_profstart_box'] = ['description_box_profstart' => $description_box_profstart, 'card_profstart' =>  $cardProfstart];
-
-// 		$i++;
-// 	}
-
-// 	return $data;
-// }
